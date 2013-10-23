f6f47306cb4faeb243cc64b44ffc046a117b4128
Jeff Williams <blueryth@gmail.com>
2013-10-23 04:47:11 -0500

    More post formatting

--- llb.py
+++ llb.py
@@ -100,8 +100,8 @@ def reply_to_submission(submission, mentioned_subs):
         for sub in mentioned_subs[:1]:
             reply += '/' + sub + ', '
     reply += '/' + mentioned_subs[-1]
-    reply += '\n\n---\nI provide direct links to lesser known cross-posted subs\
-            .\n\nLet me know if I need to try harder: /r/LazyLinkerBot'
+    reply += '\n\n---\nI provide direct links to lesser known cross-posted \
+            subs.\n\nLet me know if I need to try harder: /r/LazyLinkerBot'
     submission.add_comment(reply)
 
 # Checks if we're safe to post on a subreddit
